c7a19fd607cb737d2710ed143c7ec61a5764e64b
Carlos Pizano <cpu@chromium.org>
2014-12-30 10:55:03 -0800

    fix garbage at the end of the loaded file
    we remove the CRs form the file but in the erase-remove idiom we forgot
    the erase part.
    
    The garbage does not look like garbage, it is just part of the text
    repeated at the end so it took this long (12 days) to spot.

--- src/main.cpp
+++ src/main.cpp
@@ -225,7 +225,7 @@ private:
     auto bytes_read = file.read(block);
     // remove all CR so we only end up with LF.
     auto last = std::remove(block.start(), block.end(), '\r');
-    return plx::UTF16FromUTF8(plx::Range<const uint8_t>(block.start(), bytes_read));
+    return plx::UTF16FromUTF8(plx::Range<const uint8_t>(block.start(), last));
   }
 };
 
